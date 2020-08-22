afe06c6da39fa0bd4d959b01d6a00294f23eac58
jtu cee <leocabibihan@outlook.com>
2020-08-22 12:20:09 +0300

    test stuff

---
+++ part4/blog-list/utils/list_helper.js
@@ -0,0 +1,54 @@
+const dummy = (blogs) => 1;
+
+const totalLikes = (blogs) => blogs.reduce((acc, cur) => acc + cur.likes, 0);
+
+const favoriteBlog = (blogs) =>
+  blogs.reduce((acc, cur) => (acc < cur.likes ? cur : acc), 0);
+
+const mostBlogs = (blogs) => {
+  const frequencies = new Map();
+  blogs.forEach(({ author }) => {
+    frequencies.has(author)
+      ? frequencies.set(author, frequencies.get(author) + 1)
+      : frequencies.set(author, 1);
+  });
+  return [...frequencies.entries()].reduce(
+    (acc, cur) => (acc < cur[1] ? cur : acc),
+    []
+  )[0];
+};
+
+const mostLikes = (blogs) => {
+  const frequencies = new Map();
+  blogs.forEach((blog) => {
+    const { author, likes } = blog;
+    frequencies.has(author)
+      ? frequencies.set(author, frequencies.get(author) + likes)
+      : frequencies.set(author, likes);
+  });
+  return [...frequencies.entries()].reduce((acc, cur) =>
+    acc < cur[1] ? cur : acc
+  )[0];
+};
+
+console.log(
+  mostLikes([
+    {
+      title: "Canonical string reduction",
+      author: "Edsger W. Dijkstra",
+      likes: 12,
+    },
+    {
+      author: "mehg",
+      likes: 4,
+    },
+  ])
+);
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
+};
