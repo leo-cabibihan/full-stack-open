69b987c0426ddd21d8a12c1462bc32856af063ea
Leo <leocabibihan@outlook.com>
2020-11-18 17:41:08 +0300

    task 8.4

--- part8/graphql-server/index.js
+++ part8/graphql-server/index.js
@@ -90,7 +90,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String!): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -99,7 +99,10 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) =>
+      books.filter(
+        (book) => book.author.toLowerCase() === args.author.toLowerCase()
+      ),
     allAuthors: () =>
       authors.map((author) => {
         return {
